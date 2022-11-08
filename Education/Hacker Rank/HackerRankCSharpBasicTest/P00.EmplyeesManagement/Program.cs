cf988c7dec62ad94d0b1bcd28f53e9b1f487c719
Marti2509 <martisimov259@gmail.com>
2022-11-08 16:44:21 +0200

    Added "HackerRankCSharpBasicTest" to "Hacker Rank"

---
+++ Education/Hacker Rank/HackerRankCSharpBasicTest/P00.EmplyeesManagement/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Solution
+    {
+
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            Dictionary<string, List<Employee>> companies = new Dictionary<string, List<Employee>>();
+
+            foreach (var person in employees)
+            {
+                if (!companies.ContainsKey(person.Company))
+                {
+                    companies[person.Company] = new List<Employee>();
+                }
+
+                companies[person.Company].Add(person);
+            }
+
+            SortedDictionary<string, int> result = new SortedDictionary<string, int>();
+
+            foreach (var company in companies)
+            {
+                decimal ageSum = 0;
+                decimal count = 0;
+
+                foreach (var person in company.Value)
+                {
+                    ageSum += person.Age;
+                    count++;
+                }
+
+                result[company.Key] = (int)Math.Round((ageSum / count), 0);
+            }
+
+            Dictionary<string, int> result2 = new Dictionary<string, int>();
+
+            foreach (var company in result)
+            {
+                result2[company.Key] = company.Value;
+            }
+
+            return result2;
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            Dictionary<string, List<Employee>> companies = new Dictionary<string, List<Employee>>();
+
+            foreach (var person in employees)
+            {
+                if (!companies.ContainsKey(person.Company))
+                {
+                    companies[person.Company] = new List<Employee>();
+                }
+
+                companies[person.Company].Add(person);
+            }
+
+            SortedDictionary<string, int> result = new SortedDictionary<string, int>();
+
+            foreach (var company in companies)
+            {
+                result[company.Key] = company.Value.Count;
+            }
+
+            Dictionary<string, int> result2 = new Dictionary<string, int>();
+
+            foreach (var company in result)
+            {
+                result2[company.Key] = company.Value;
+            }
+
+            return result2;
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            Dictionary<string, List<Employee>> companies = new Dictionary<string, List<Employee>>();
+
+            foreach (var person in employees)
+            {
+                if (!companies.ContainsKey(person.Company))
+                {
+                    companies[person.Company] = new List<Employee>();
+                }
+
+                companies[person.Company].Add(person);
+            }
+
+            SortedDictionary<string, Employee> result = new SortedDictionary<string, Employee>();
+
+            foreach (var company in companies)
+            {
+                Employee employee = new Employee();
+                int age = 0;
+
+                foreach (var person in company.Value)
+                {
+                    if (person.Age > age)
+                    {
+                        age = person.Age;
+                        employee = person;
+                    }
+                }
+
+                result[company.Key] = employee;
+            }
+
+            Dictionary<string, Employee> result2 = new Dictionary<string, Employee>();
+
+            foreach (var company in result)
+            {
+                result2[company.Key] = company.Value;
+            }
+
+            return result2;
+        }
+
+        public static void Main()
+        {
+
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
---
+++ Education/Hacker Rank/HackerRankCSharpBasicTest/P01.TeamInheritance/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+
+    public class Team
+    {
+        public Team(string teamName, int noOfPlayers)
+        {
+            this.teamName = teamName;
+            this.noOfPlayers = noOfPlayers;
+        }
+
+        public string teamName { get; set; }
+        public int noOfPlayers { get; set; }
+
+        public void AddPlayer(int count)
+        {
+            this.noOfPlayers += count;
+        }
+
+        public bool RemovePlayer(int count)
+        {
+            if (this.noOfPlayers - count < 0)
+            {
+                return false;
+            }
+            else
+            {
+                this.noOfPlayers -= count;
+                return true;
+            }
+        }
+    }
+
+    public class Subteam : Team
+    {
+        public Subteam(string teamName, int noOfPlayers)
+            : base(teamName, noOfPlayers) { }
+
+        public void ChangeTeamName(string name)
+        {
+            this.teamName = name;
+        }
+    }
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+
+            if (!typeof(Subteam).IsSubclassOf(typeof(Team)))
+            {
+                throw new Exception("Subteam class should inherit from Team class");
+            }
+
+            String str = Console.ReadLine();
+            String[] strArr = str.Split();
+            string initialName = strArr[0];
+            int count = Convert.ToInt32(strArr[1]);
+            Subteam teamObj = new Subteam(initialName, count);
+            Console.WriteLine("Team " + teamObj.teamName + " created");
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            teamObj.AddPlayer(count);
+            Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+
+
+            str = Console.ReadLine();
+            count = Convert.ToInt32(str);
+            Console.WriteLine("Current number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            var res = teamObj.RemovePlayer(count);
+            if (res)
+            {
+                Console.WriteLine("New number of players in team " + teamObj.teamName + " is " + teamObj.noOfPlayers);
+            }
+            else
+            {
+                Console.WriteLine("Number of players in team " + teamObj.teamName + " remains same");
+            }
+
+            str = Console.ReadLine();
+            teamObj.ChangeTeamName(str);
+            Console.WriteLine("Team name of team " + initialName + " changed to " + teamObj.teamName);
+        }
+    }
+}
