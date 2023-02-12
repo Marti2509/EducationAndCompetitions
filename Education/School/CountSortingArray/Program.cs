4051867dc2d289928165c1448814b066ea639e0c
Marti2509 <martisimov259@gmail.com>
2023-02-12 17:54:07 +0200

    Added "CountSortingArray"

---
+++ Education/School/CountSortingArray/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] rawNums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+Dictionary<int, int> keyValue = new Dictionary<int, int>();
+List<int> list = new List<int>();
+
+foreach (int currNum in rawNums)
+{
+    if (keyValue.ContainsKey(currNum))
+    {
+        keyValue[currNum]++;
+    }
+    else
+    {
+        keyValue.Add(currNum, 1);
+    }
+}
+
+for (int i = 0; i < rawNums.Length; i++)
+{
+    int currBigestNum = int.MinValue;
+    int currBigestValue = int.MinValue;
+
+    foreach (var item in keyValue)
+    {
+        if (item.Value > currBigestValue)
+        {
+            currBigestValue = item.Value;
+            currBigestNum = item.Key;
+        }
+        else if (item.Value == currBigestValue)
+        {
+            if (item.Key < currBigestNum)
+            {
+                currBigestValue = item.Value;
+                currBigestNum = item.Key;
+            }
+        }
+    }
+
+    for (int k = 0; k < currBigestValue; k++)
+    {
+        list.Add(currBigestNum);
+    }
+
+    keyValue.Remove(currBigestNum);
+}
+
+Console.WriteLine(string.Join(" ", list));
